ebb3ebb614254d30bcccdeccba238e328920099c
Chigozie-css <80043928+Chigozie-css@users.noreply.github.com>
2022-12-01 18:18:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
-const themeToggler = document.querySelector(".theme-Toggler");
+const themeToggler = document.querySelector(".theme-toggler");
 
 // Show Sidebar
 menuBtn.addEventListener('click', () => {
@@ -15,7 +15,7 @@ closeBtn.addEventListener('click', () => {
 
 // Change Theme
 themeToggler.addEventListener('click', () => {
-    document.body.classList.toggle('.dark-theme-variables');
+    document.body.classList.toggle('dark-theme-variables');
 
     themeToggler.querySelector('span:nth-child(1)').
     classList.toggle('active');
@@ -27,13 +27,14 @@ themeToggler.addEventListener('click', () => {
 Orders.forEach(order => {
     const tr = document.createElement('tr');
     const trContent = `
-                        <td>${order.productName}</td>
+                        <td>${order.productName} </td>
                         <td>${order.productNumber}</td>
                         <td>${order.paymentStatus}</td>
                         <td class="${order.shipping ===
                         'Declined' ? 'danger' : order.
                         shipping === 'pending' ? 
-                        'warning' : 'primary'}">${order.shipping}<
+                        'warning' 
+                        : 'primary'}">${order.shipping}<
                         /td>
                         <td class="primary">Details</td>
                        `;
